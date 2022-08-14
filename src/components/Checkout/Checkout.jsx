b62ec041f2c8c69abfa709f46820b02a06992ab2
dnmartel <marteld1992@gmail.com>
2022-08-14 11:25:24 -0300

    Checkout validation

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -12,22 +12,28 @@ const Checkout = () => {
 
     const { cart, cartTotal, clearCart } = useContext(CartContext)
 
-    const [order, setOrder] = useState(
-        {
-            buyer: {
-                name: "",
-                lastname: "",
-                email: "",
-                confirmEmail: "",
-                telephone: ""
-            },
-            date: new Date(),
-            items: cart.map(p => ({ id: p.id, title: p.title })),
-            total: cartTotal()
-        }
-    )
+    const [error, setError] = useState({
+        name: false,
+        lastname: false,
+        email: false,
+        confirmEmail: false,
+        telephone: false
+    });
 
-    const [orderId, setOrderId] = useState(null)
+    const [order, setOrder] = useState({
+        buyer: {
+            name: "",
+            lastname: "",
+            email: "",
+            confirmEmail: "",
+            telephone: ""
+        },
+        date: new Date(),
+        items: cart.map(p => ({ id: p.id, title: p.title })),
+        total: cartTotal()
+    });
+
+    const [orderId, setOrderId] = useState(null);
 
     const handleChange = (e) => {
 
@@ -40,91 +46,124 @@ const Checkout = () => {
             }
         }))
 
-    }
+    };
 
     const validateForm = () => {
-        let validated = false;
+        let validate, vName, vLastName, vEmail, vConfirmEmail, vTelephone = false;
 
         if (order.buyer.name.length === 0) {
-            console.log(order.buyer.name + order.buyer.name.length);
-            validated = false;
+            setError((currentValue) => ({
+                ...currentValue, name: true
+            }))
+            vName = false;
+        } else {
+            setError((currentValue) => ({
+                ...currentValue, name: false
+            }))
+            vName = true;
         }
 
         if (order.buyer.lastname.length === 0) {
-            console.log(order.buyer.lastname+order.buyer.lastname.length);
-            validated = false;
+            setError((currentValue) => ({
+                ...currentValue, lastname: true
+            }))
+            vLastName = false;
+        } else {
+            setError((currentValue) => ({
+                ...currentValue, lastname: false
+            }))
+            vLastName = true;
         }
 
         if (order.buyer.email.length === 0) {
-            console.log(order.buyer.email+order.buyer.email.length);
-            validated = false;
+            setError((currentValue) => ({
+                ...currentValue, email: true
+            }))
+            vEmail = false;
+        } else {
+            setError((currentValue) => ({
+                ...currentValue, email: false
+            }))
+            vEmail = true;
         }
 
         if (order.buyer.email !== order.buyer.confirmEmail) {
-            console.log("los correos no coinciden");
-            validated = false;
+            setError((currentValue) => ({
+                ...currentValue, confirmEmail: true
+            }))
+            vConfirmEmail = false;
+        } else {
+            setError((currentValue) => ({
+                ...currentValue, confirmEmail: false
+            }))
+            vConfirmEmail = true;
         }
 
         if (order.buyer.telephone.length === 0) {
-            console.log(order.buyer.telephone+order.buyer.telephone.length);
-            validated = false;
+            setError((currentValue) => ({
+                ...currentValue, telephone: true
+            }))
+            vTelephone = false;
+        } else {
+            setError((currentValue) => ({
+                ...currentValue, telephone: false
+            }))
+            vTelephone = true;
+        }
+
+        if ( vName || vLastName || vEmail || vConfirmEmail || vTelephone) {
+            validate = false;
+        } else {
+            validate = true;
         }
 
 
-        return validated;
+        return validate;
 
-    }
+    };
 
     const handleClick = () => {
-
         if (validateForm()) {
 
             const db = getFirestore();
-        const ordersCollection = collection(db, "Orders");
-        addDoc(ordersCollection, order)
-            .then(({ id }) => {
-                console.log(id + ` Success`);
-                clearCart();
-                setOrderId(id)
-            });
-            
+            const ordersCollection = collection(db, "Orders");
+            addDoc(ordersCollection, order)
+                .then(({ id }) => {
+                    console.log(id + ` Success`);
+                    clearCart();
+                    setOrderId(id)
+                });
+
         }
-        
-    }
+    };
 
     if (orderId) {
         return <Success id={orderId} />
-    }
+    };
 
     if (cart.length === 0) {
         return <Navigate to="/" />
-    }
-
+    };
 
     return (
         <>
             <div className="App">
                 <form className='formCompra'>
-                    <input type="text" id="name" name="name" placeholder="Nombre" value={order.buyer.name} onChange={(e) => {
-                        handleChange(e)
-                    }} />
+                    <input type="text" id="name" name="name" placeholder="Nombre" value={order.buyer.name} onChange={(e) => { handleChange(e) }} />
+                    {error.name && <p className="formEmailError">El campo no puede estar vacio</p>}
 
-                    <input type="text" id="lastname" name="lastname" placeholder="Apellido" value={order.buyer.lastname} onChange={(e) => {
-                        handleChange(e)
-                    }} />
+                    <input type="text" id="lastname" name="lastname" placeholder="Apellido" value={order.buyer.lastname} onChange={(e) => { handleChange(e) }} />
+                    {error.lastname && <p className="formEmailError">El campo no puede estar vacio</p>}
 
-                    <input type="email" id="email" name="email" placeholder="Correo" value={order.buyer.email} onChange={(e) => {
-                        handleChange(e)
-                    }} />
-
-                    <input type="email" id="confirmEmail" name="confirmEmail" placeholder="Repetir correo" value={order.buyer.confirmEmail} onChange={(e) => {
-                        handleChange(e)
-                    }} />
+                    <input type="email" id="email" name="email" placeholder="Correo" value={order.buyer.email} onChange={(e) => { handleChange(e) }} />
+                    {error.email && <p className="formEmailError">El campo no puede estar vacio</p>}
                     {order.buyer.email !== order.buyer.confirmEmail && <p className="formEmailError">No coinciden los correos</p>}
 
-                    <input type="number" id="telephone" placeholder="Teléfono" value={order.buyer.telephone} onChange={(e) => {
-                        handleChange(e)
-                    }} />
+                    <input type="email" id="confirmEmail" name="confirmEmail" placeholder="Repetir correo" value={order.buyer.confirmEmail} onChange={(e) => { handleChange(e) }} />
+                    {order.buyer.email !== order.buyer.confirmEmail && <p className="formEmailError">No coinciden los correos</p>}
+
+                    <input type="number" id="telephone" placeholder="Teléfono" value={order.buyer.telephone} onChange={(e) => { handleChange(e) }} />
+                    {error.telephone && <p className="formEmailError">El campo no puede estar vacio</p>}
 
                 </form>
                 <div id="btnContainer">
@@ -133,6 +172,6 @@ const Checkout = () => {
             </div>
             <Footer /></>
     )
-}
+};
 
 export default Checkout;
